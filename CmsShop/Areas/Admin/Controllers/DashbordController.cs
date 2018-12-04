ceb3b83b81fe47a1cc97e64a937aa56b8d8982d0
Damian <damian.krzem@gmail.com>
2018-12-04 20:36:40 +0100

    Admin Update

---
+++ CmsShop/Areas/Admin/Controllers/DashbordController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace CmsShop.Areas.Admin.Controllers
+{
+    public class DashbordController : Controller
+    {
+        // GET: Admin/Dashbord
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CmsShop/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using CmsShop.Models.Data;
+using CmsShop.Models.ViewModels.Pages;
+
+namespace CmsShop.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            //Deklaracja listy PageViewModel
+            List<PageViewModel> pagesList;
+
+            //Inicjalizacja listy
+            using (Db db = new Db())
+            {
+                pagesList = db.Pages.ToArray()
+                    .OrderBy(x => x.Sorting)
+                    .Select(x => new PageViewModel(x))
+                    .ToList();
+            }
+
+            // zwracamy strony do widoku
+            return View(pagesList);
+        }
+    }
+}
