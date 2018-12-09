92af3855825e266d6d6b6dba890f7052a0a1e940
Damian <damian.krzem@gmail.com>
2018-12-09 14:19:03 +0100

    Update kolejnej lekcji Admin Shop

--- CmsShop/Areas/Admin/Controllers/ShopController.cs
+++ CmsShop/Areas/Admin/Controllers/ShopController.cs
@@ -8,7 +8,8 @@ namespace CmsShop.Areas.Admin.Controllers
 {
     public class ShopController : Controller
     {
-        // GET: Admin/Shop/Categories
+        //GET: Admin/Shop/Categories
+        [HttpGet]
         public ActionResult Categories()
         {
             //deklaracja listy kategorii do wyświetlenia
@@ -24,6 +25,109 @@ namespace CmsShop.Areas.Admin.Controllers
             }
 
             return View(categoryViewModelList);
+        }
+
+        //POST: Admin/Shop/AddNewCategory
+        [HttpPost]
+        public string AddNewCategory(string catName)
+        {
+            //Deklaracja id
+            string id;
+
+            using (Db db = new Db())
+            {
+                //sprawdzenie czy kategoria jest unikalna
+                if (db.Categories.Any(x => x.Name == catName))
+                {
+                    return "tytulzajety";
+                }
+
+                //inicjalizacja DTO
+                CategoryDTO dto = new CategoryDTO();
+                dto.Name = catName;
+                dto.Slug = catName.Replace(" ", "-").ToLower();
+                dto.Sorting = 1000;
+                
+                //zapis do bazy
+                db.Categories.Add(dto);
+                db.SaveChanges();
+
+                //pobieramy id
+                id = dto.Id.ToString();
+            }
+
+            return id;
+        }
+
+        //POST: Admin/Shope/ReorderCategories
+        [HttpPost]
+        public ActionResult ReorderCategories(int[] id)
+        {
+            using (Db db = new Db())
+            {
+                // inicjalizacja licznika
+                int count = 1;
+               
+                // deklaracja DTO
+                CategoryDTO dto;
+                
+                //Sortowanie kategorii
+                foreach (var catId in id)
+                {
+                    dto = db.Categories.Find(catId);
+                    dto.Sorting = count;
+                    db.SaveChanges();
+                    count++;
+                }
+            }
+            
+            return View();
+        }
+
+        //GET: Admin/Shop/DeleteCategory
+        [HttpGet]
+        public ActionResult DeleteCategory(int id)
+        {
+            using (Db db = new Db())
+            {
+                //pobieramy kategorię
+                CategoryDTO dto = db.Categories.Find(id);
+
+                //usuwanie kategorii
+                db.Categories.Remove(dto);
+
+                //zapis na bazie
+                db.SaveChanges();
+
+            }
+                return RedirectToAction("Categories");
+        }
+
+
+        //POST: Admin/Shop/RenameCategory
+        [HttpPost]
+        public string RenameCategory(string newCatName, int id)
+        {
+            using (Db db = new Db())
+            {
+                //sprawdzenie czy kategoria jest unikatowa
+                if (db.Categories.Any((x => x.Name == newCatName)))
+                {
+                    return "tytulzajety";
+                }
+
+                //pobranie i edycja kategorii
+                CategoryDTO dto = db.Categories.Find(id);
+
+                //edycja kategorii
+                dto.Name = newCatName;
+                dto.Slug = newCatName.Replace(" ", "-").ToLower();
+
+                // zapis na bazie
+                db.SaveChanges();
+            }
+
+            return "OK";
         }
     }
 }
